042e6921ffb51e52c21383e54e02ff345817e5df
prakashtaz0091 <code.ing2468@gmail.com>
2025-12-30 18:10:41 +0545

    prod minimal requirements

--- project/urls.py
+++ project/urls.py
@@ -2,15 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from debug_toolbar.toolbar import debug_toolbar_urls
 
 
-urlpatterns = (
-    [
-        path("admin/", admin.site.urls),
-        path("accounts/", include("accounts.urls")),
-        path("", include("store.urls")),
-    ]
-    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    + debug_toolbar_urls()
-)
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("accounts/", include("accounts.urls")),
+    path("", include("store.urls")),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
